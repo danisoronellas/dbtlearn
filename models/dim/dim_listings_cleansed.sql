09f6d7be7cfb30c2b514f610f76f740eda8979be
danisoronellas <daniel.soronellas@gmail.com>
2024-07-31 22:38:22 +0200

    created core layer

---
+++ models/dim/dim_listings_cleansed.sql
@@ -0,0 +1,23 @@
+with src_listings as (
+    select * from {{ ref('src_listings') }}
+)
+select
+    listing_id,
+    listing_name,
+    ROOM_TYPE,
+    case
+        when MINIMUM_NIGHTS = 0 then 1
+        else MINIMUM_NIGHTS
+    end as minimum_nights,
+    HOST_ID,
+    replace(
+        price_str,
+        '$'
+    ) :: number(
+        10,
+        2
+    ) as price,
+    CREATED_AT,
+    UPDATED_AT
+from
+    src_listings
